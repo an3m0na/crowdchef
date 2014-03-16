0330721623bc8d847f7e26a4086e234c2de6bfd4
Maria Voinea <M.Voinea@student.tudelft.nl>
2014-03-16 19:10:24 +0100

    Playing with web service

--- crowdchef-webservice/src/main/java/com/crowdchef/webservice/WebService.java
+++ crowdchef-webservice/src/main/java/com/crowdchef/webservice/WebService.java
@@ -1,8 +1,10 @@
 package com.crowdchef.webservice;
 
+import org.eclipse.jetty.http.HttpStatus;
 import spark.*;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class WebService {
     public void start()
@@ -13,6 +15,27 @@ public class WebService {
             public Object handle(final Request request, final Response response)
             {
                 return "CrowdChefServer is On!";
+            }
+        });
+
+        get(new Route("/ane/puiumeu")
+        {
+            @Override
+            public Object handle(final Request request, final Response response)
+            {
+                request.params("chilotei");
+                return "Pasarica    "+request.queryParams("chilotei");
+            }
+        });
+
+
+        post(new Route("/ane/post") {
+            @Override
+            public Object handle(final Request request, final Response response) {
+                request.body();
+                System.out.println(request.body());
+                request.params("chilotei");
+                return HttpStatus.ACCEPTED_202;
             }
         });
 
