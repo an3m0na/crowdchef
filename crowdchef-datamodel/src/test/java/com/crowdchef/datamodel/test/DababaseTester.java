2d283da01361bca4bba8c663499bff6c6a5453a7
Maria Voinea <M.Voinea@student.tudelft.nl>
2014-03-16 14:06:50 +0100

    Add User and UserInfo logic

--- crowdchef-datamodel/src/test/java/com/crowdchef/datamodel/test/DababaseTester.java
+++ crowdchef-datamodel/src/test/java/com/crowdchef/datamodel/test/DababaseTester.java
@@ -3,6 +3,8 @@ package com.crowdchef.datamodel.test;
 import com.crowdchef.datamodel.CrowdChefDatabase;
 import com.crowdchef.datamodel.DatabaseUtil;
 import com.crowdchef.datamodel.entities.Recipe;
+import com.crowdchef.datamodel.entities.User;
+import com.crowdchef.datamodel.entities.UserInfo;
 import org.hibernate.criterion.Restrictions;
 
 import javax.xml.transform.Result;
@@ -10,6 +12,23 @@ import java.util.List;
 
 
 public class DababaseTester {
+
+    public static void populateDatabase(CrowdChefDatabase database){
+        User newUser = new User("john", "john");
+        UserInfo newUserInfo= new UserInfo(newUser, "john@gmail.com");
+        newUserInfo.setAddress("Some Street 124");
+        newUserInfo.setCity("Delft");
+        newUserInfo.setCountry("Netherlands");
+        database.save(newUser, User.class);
+        database.save(newUserInfo, UserInfo.class);
+
+        Recipe newRecipe = new Recipe("Perfect recipe", newUser);
+        newRecipe.setDescription("Awesome recipe for a recipe");
+        newRecipe.setDirections("Stir recipe until becomes recipe");
+        newRecipe.setTags("perfect,inception");
+        database.save(newRecipe, Recipe.class);
+    }
+
     public  static void main (String[] args){
         CrowdChefDatabase database = new CrowdChefDatabase();
 
@@ -25,11 +44,9 @@ public class DababaseTester {
 //        }
 //        System.out.println(recipe.getId());
 
-        List<Recipe> recipes = database.retrieve("AllRecipesLikeName", "name", "perfect recipe for succes", Recipe.class);
-        System.out.println(recipes);
-        Recipe newRecipe = new Recipe("Second recipe");
-        database.save(newRecipe, Recipe.class);
-        recipes = database.retrieve("AllRecipesLikeName", "name", "perfect recipe for succes", Recipe.class);
+        populateDatabase(database);
+
+        List<Recipe> recipes = database.retrieve("AllRecipes", Recipe.class);
         System.out.println(recipes);
     }
 }
